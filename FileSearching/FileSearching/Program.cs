b31eec99b2ade8f1c4366375fe06ba4489c6f815
Xien Singh <xien@ix.co.za>
2019-01-17 13:53:36 +0200

    Search big files for specific enteries, reads line by line, not loading everything into RAM

---
+++ FileSearching/FileSearching/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileSearching
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string liness;
+            StreamReader file = new StreamReader(@"YourLogFile.log");
+            while ((liness = file.ReadLine()) != null)
+            {
+                if (liness.Contains("conditon1") || liness.Contains("conditon2") || liness.Contains("conditon3"))
+                {
+                    using (StreamWriter filewrite =
+                    new StreamWriter(@"yourOutputfile.txt", true))
+                    {
+                        filewrite.WriteLine(liness);
+                    }
+                    Console.WriteLine(liness);
+                }
+            }
+            file.Close();
+            Console.ReadLine();
+        }
+    }
+}
