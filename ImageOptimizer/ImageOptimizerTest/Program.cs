e8514e3f4b77dd92fe19fafd8a31f89d0a880902
Xien Singh <xiensingh007@gmail.com>
2020-06-06 19:45:37 +0200

    Single commit for entire project, was smaller and easier than expected, there are some stuff still to be done, but I am working on it

--- ImageOptimizer/ImageOptimizerTest/Program.cs
+++ ImageOptimizer/ImageOptimizerTest/Program.cs
@@ -3,13 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using ImageOptimizerLogic;
 namespace ImageOptimizerTest
 {
 	class Program
 	{
 		static void Main(string[] args)
 		{
+			compressor compressor = new compressor();
+			Console.WriteLine(DateTime.Now);
+			compressor.CompressImage(@"PathToImages",90);
+			Console.ReadLine();
 		}
 	}
 }
