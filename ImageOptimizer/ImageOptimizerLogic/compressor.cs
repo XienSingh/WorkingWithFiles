0f6275fbab17fb7d27778df5f310f7bc612fd294
Xien Singh <xiensingh007@gmail.com>
2020-06-08 08:55:39 +0200

    Some Monday Morning Refactoring

--- ImageOptimizer/ImageOptimizerLogic/compressor.cs
+++ ImageOptimizer/ImageOptimizerLogic/compressor.cs
@@ -9,82 +9,75 @@ using System.Threading.Tasks;
 
 namespace ImageOptimizerLogic
 {
-	public class compressor
+	public class Compressor
 	{
     public static void CompressImage(string SoucePath, int quality)
     {
-      int count = 0;
-      List<string> Directories = Directory.GetDirectories(SoucePath).ToList();
-      List<List<string>> SecondLvlDir = new List<List<string>>();
-      List<List<string>> images = new List<List<string>>();
-      foreach (string path in Directories)
-      {
-        SecondLvlDir.Add(Directory.GetDirectories(path).ToList());
-      }
-      foreach (List<string> paths in SecondLvlDir)
-			{
-				foreach (string item in paths) { 
-        images.Add(Directory.EnumerateFiles(item).ToList());
-        }
-      }  
-      foreach(List<string> items in images)
-      {
-        foreach(string imgPath in items) 
-        { 
-        var FileName = Path.GetFileName(imgPath);
-        var extension = Path.GetExtension(FileName);
-        var DestPath = imgPath;
-        FileStream fs = new FileStream(imgPath, FileMode.Open);
-        Image imgPhoto = Image.FromStream(fs);
-          Console.WriteLine("------------------------------------------------------------------------------");
-        Console.ForegroundColor = ConsoleColor.Cyan;
-        Console.WriteLine($"Image to be optimised: {FileName} original size = {fs.Length/1000} Kb");
 
-          fs.Close();
+      IEnumerable<string> allIamges = Directory.GetFiles(SoucePath, "*.*", SearchOption.AllDirectories);
+      int count = 0;
+      double origSize = 0;
+      double SavedSize = 0;
+      foreach(string item in allIamges)
+      {
+        var FileName = Path.GetFileName(item);
+        var extension = Path.GetExtension(FileName);
+        var DestPath = item;
+        FileStream fs = new FileStream(item, FileMode.Open);
+        Image imgPhoto = Image.FromStream(fs);
+        Console.WriteLine("------------------------------------------------------------------------------");
+        Console.ForegroundColor = ConsoleColor.Cyan;
+        double originalImageSize = fs.Length/1000;
+        Console.WriteLine($"Image to be optimised: {FileName} original size = {originalImageSize} Kb");
+        origSize += originalImageSize;
+        fs.Close();
         using (Bitmap bmp1 = new Bitmap(imgPhoto))
         {
-            ImageFormat imageFormat = null;
-            switch (extension)
-            {
-              case "jpeg":
-              case "jpg":
-                imageFormat = ImageFormat.Jpeg;
-                break;
-              case "png":
-                imageFormat = ImageFormat.Png;
-                break;
-              case "bmp":
-                imageFormat = ImageFormat.Bmp;
-                break;
-              case "gif":
-                imageFormat = ImageFormat.Gif;
-                break;
-               default:
-                imageFormat = ImageFormat.Jpeg;
-                break;
-						}
-            Console.ForegroundColor = ConsoleColor.Magenta;
+          ImageFormat imageFormat = null;
+          switch (extension)
+          {
+            case "jpeg":
+            case "jpg":
+              imageFormat = ImageFormat.Jpeg;
+              break;
+            case "png":
+              imageFormat = ImageFormat.Png;
+              break;
+            case "bmp":
+              imageFormat = ImageFormat.Bmp;
+              break;
+            case "gif":
+              imageFormat = ImageFormat.Gif;
+              break;
+            default:
+              imageFormat = ImageFormat.Jpeg;
+              break;
+          }
+          Console.ForegroundColor = ConsoleColor.Magenta;
           Console.WriteLine($"Image Name: {FileName}");
-            Console.ForegroundColor = ConsoleColor.Yellow;
-            Console.WriteLine("Processing");
+          Console.ForegroundColor = ConsoleColor.Yellow;
+          Console.WriteLine("Processing");
           ImageCodecInfo jpgEncoder = GetEncoder(imageFormat);
           System.Drawing.Imaging.Encoder QualityEncoder = System.Drawing.Imaging.Encoder.Quality;
           EncoderParameters myEncoderParameters = new EncoderParameters(1);
           EncoderParameter myEncoderParameter = new EncoderParameter(QualityEncoder, quality);
           myEncoderParameters.Param[0] = myEncoderParameter;
           bmp1.Save(DestPath, jpgEncoder, myEncoderParameters);
-            count++;
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine($"Image optimised: {FileName} original size = {(new FileInfo(DestPath)).Length/1000} Kb");
-            Console.WriteLine("------------------------------------------------------------------------------");
+          count++;
+          Console.ForegroundColor = ConsoleColor.Green;
+          double optimisedSize = (new FileInfo(DestPath)).Length / 1000;
+          Console.WriteLine($"Image optimised: {FileName} original size = {optimisedSize} Kb");
+          Console.WriteLine("------------------------------------------------------------------------------");
+          SavedSize += optimisedSize;
+          Console.ResetColor();
 
-            Console.ResetColor();
-
-					}
         }
-			}
-      Console.WriteLine("Done");
+      }
+    
+    Console.WriteLine("Done");
       Console.WriteLine($"Total number of images processed : {count}");
+      Console.WriteLine($"Original Folder size {origSize/1000} Mb");
+      Console.WriteLine($"New Folder size {SavedSize/1000} Mb");
       Console.WriteLine(DateTime.Now);
     }
     private static ImageCodecInfo GetEncoder(ImageFormat format)
--- ImageOptimizer/ImageOptimizerTest/Program.cs
+++ ImageOptimizer/ImageOptimizerTest/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,9 +11,15 @@ namespace ImageOptimizerTest
 	{
 		static void Main(string[] args)
 		{
-			compressor compressor = new compressor();
-			Console.WriteLine(DateTime.Now);
-			compressor.CompressImage(@"PathToImages",90);
+			Stopwatch stopWatch = new Stopwatch();
+			stopWatch.Start();
+			Compressor.CompressImage(@"PathToImages", 100);
+			stopWatch.Stop();
+			TimeSpan ts = stopWatch.Elapsed;
+			string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+			ts.Hours, ts.Minutes, ts.Seconds,
+			ts.Milliseconds / 10);
+			Console.WriteLine("RunTime " + elapsedTime);
 			Console.ReadLine();
 		}
 	}
